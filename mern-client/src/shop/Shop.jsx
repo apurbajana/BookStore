0dc355b4fc3c4b31ac62481c0b2650a9aeb6c004
apurbajana <janaapurba24@gmail.com>
2024-05-12 12:22:34 +0530

    Update Shop.jsx

--- mern-client/src/shop/Shop.jsx
+++ mern-client/src/shop/Shop.jsx
@@ -3,7 +3,7 @@ import { Card } from 'flowbite-react';
 export default function Shop() {
   const [books,setBooks]=useState([]);
   useEffect(()=>{
-    fetch(`http://localhost:5000/all-books`).then(res=>res.json()).then(data=>setBooks(data));
+    fetch(`https://bookstore-server-m0wg.onrender.com/all-books`).then(res=>res.json()).then(data=>setBooks(data));
   },[])
   return (
     <div className='mt-28 px-4 lg:px-24'>
