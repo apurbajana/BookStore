bdc5cbde264387ea8d981fc737bd51952b6df989
apurbajana <janaapurba24@gmail.com>
2024-05-12 12:24:20 +0530

    Update BestSellerBooks.jsx

--- mern-client/src/home/BestSellerBooks.jsx
+++ mern-client/src/home/BestSellerBooks.jsx
@@ -4,7 +4,7 @@ import BookCards from '../components/BookCards';
 export default function BestSellerBooks() {
     const [books,setBooks]=useState([]);
     useEffect(()=>{
-        fetch("http://localhost:5000/all-books").then(res=>res.json()).then(data=>setBooks(data.slice(0,8)))
+        fetch("https://bookstore-server-m0wg.onrender.com/all-books").then(res=>res.json()).then(data=>setBooks(data.slice(0,8)))
     },[])
   return (
     <div>
