ce57297af39d73d990f4650a30baf97ba121f63f
apurbajana <janaapurba24@gmail.com>
2024-05-12 12:24:49 +0530

    Update OtherBooks.jsx

--- mern-client/src/home/OtherBooks.jsx
+++ mern-client/src/home/OtherBooks.jsx
@@ -4,7 +4,7 @@ import BookCards from '../components/BookCards';
 export default function OtherBooks() {
     const [books,setBooks]=useState([]);
     useEffect(()=>{
-        fetch("http://localhost:5000/all-books").then(res=>res.json()).then(data=>setBooks(data.slice(0,4)))
+        fetch("https://bookstore-server-m0wg.onrender.com/all-books").then(res=>res.json()).then(data=>setBooks(data.slice(0,4)))
     },[])
   return (
     <div>
